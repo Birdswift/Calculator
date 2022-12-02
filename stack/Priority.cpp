c826e059de00bd18a1fcb39ed3d6021e50817356
Birdswift <112816261+Birdswift@users.noreply.github.com>
2022-12-02 09:23:18 +0300

    Update Priority.cpp

--- stack/Priority.cpp
+++ stack/Priority.cpp
@@ -6,14 +6,14 @@
 int priorityofelem(char check) {
 	int level = 0;
 	std::map <char, int> priority;
-	priority.insert({ '+',1 });
-	priority.insert({ '-',1 });
-	priority.insert({ '*',2 });
-	priority.insert({ '/',2 });
-	priority.insert({ 's',3 });
-	priority.insert({ 'c',3 });
-	priority.insert({ 't',3 });
-	priority.insert({ 'o',3 });
+	priority.insert({'+',1});
+	priority.insert({'-',1});
+	priority.insert({'*',2});
+	priority.insert({'/',2});
+	priority.insert({'s',3});
+	priority.insert({'c',3});
+	priority.insert({'t',3});
+	priority.insert({'o',3});
 	for (auto& i : priority) {
 		if (check == i.first) {
 			level = i.second;
